fa873c50a36232d9c66871b4257c562e71bc56e5
Manoj-Kurisheti <manoj.kurisheti222@gmail.com>
2023-12-14 13:45:08 +0530

    2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -2,7 +2,7 @@
 p1=Ramesh
 p2=Suresh
 
-echo "$person1:Hello $person2,Good Morning"
-echo "$person2:Hi $person1,Very Good morning"
-echo "$person1:How are you $person2"
-echo "$person2:Iam good $person1,How about you"
+echo "$p1:Hello $p2,Good Morning"
+echo "$p2:Hi $p1,Very Good morning"
+echo "$p1:How are you $p2"
+echo "$p2:Iam good $p1,How about you"
