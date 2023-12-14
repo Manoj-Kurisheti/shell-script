1b2257d58e712e7b416b969e1d55ac157cf4edbd
Manoj-Kurisheti <manoj.kurisheti222@gmail.com>
2023-12-14 13:36:29 +0530

    2nd script

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+person1=Ramesh
+person2=Suresh
+
+echo "$person1:Hello $person2,Good Morning"
+echo "$person2:Hi $person1,Very Good morning"
+echo "$person1:How are you $person2"
+echo "$person2:Iam good $person1,How about you"
